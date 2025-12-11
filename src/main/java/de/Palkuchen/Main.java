7d09d266c3df0262703bc37a5cc3d78899930f80
Laplace <palkuchen@gmail.com>
2025-12-11 18:42:46 +0100

    Initial Commit

--- src/main/java/de/Palkuchen/Main.java
+++ src/main/java/de/Palkuchen/Main.java
@@ -1,9 +1,28 @@
 package de.Palkuchen;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Haffmann
+        String input = "in ulm und um ulm herrum";
+        HashMap<Character, Integer> result = getCountOfChars(input);
+
+
+    }
+
+    public static HashMap<Character, Integer> getCountOfChars(String input) {
+        HashMap<Character, Integer> result = new HashMap<>();
+
+        for (char c1 : input.toCharArray()) {
+            if (result.containsKey(c1)) continue;
+            int count = 0;
+            for (char c2 : input.toCharArray()) {
+                if (c2 == c1) count++;
+            }
+            result.put(c1, count);
+        }
+
+        return result;
     }
 }
